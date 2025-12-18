6f747a94d9de5128cbe3e134ba7d2fc9e4d2cb05
Iaroslav Bratovnikov <bratovnikoff@gmail.com>
2025-12-18 04:54:43 +0300

    Add problem #2648

---
+++ 2648.js
@@ -0,0 +1,21 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function* () {
+    let prev = 0;
+    let current = 1;
+    for (let i = 0; i < Infinity; i++) {
+        yield prev;
+        let tmp = current;
+        current += prev;
+        prev = tmp;
+    }
+
+    return prev;
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
