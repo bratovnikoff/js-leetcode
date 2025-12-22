14f31c24de4038cf57feca204c7c0e262f45b964
Iaroslav Bratovnikov <bratovnikoff@gmail.com>
2025-12-22 03:38:44 +0300

    Add 2 problems

---
+++ 2630.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const memoized = new Map();
+
+    return function (...args) {
+        let currentMap = memoized;
+
+        for (let i = 0; i < args.length; i++) {
+            if (!currentMap.has(args[i])) currentMap.set(args[i], new Map());
+
+            currentMap = currentMap.get(args[i]);
+        }
+
+        if (!currentMap.has("fn")) currentMap.set("fn", fn(...args));
+
+        return currentMap.get("fn");
+    };
+}
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
